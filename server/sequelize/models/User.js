41c2f3289d53170705fd112e33358494ccba8bcd
Lisandro Veron <lisandro.veron@hotmail.com>
2024-03-25 21:10:55 -0300

    Server ->
    Added:
    - Now user's password is hashed and salted before creation.

--- server/sequelize/models/User.js
+++ server/sequelize/models/User.js
@@ -1,5 +1,6 @@
 import {DataTypes, Model} from "sequelize";
 import sequelize from "../config.js";
+import {createHash, randomBytes} from "crypto";
 
 class User extends Model {};
 
@@ -32,17 +33,30 @@ User.init({
       isEmail: true
     }
   },
+  salt: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
   password: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
       len: [8, 255],
+    },
+    set(password) {
+      const hash = createHash("sha256");
+      hash.update(password + this.salt);
+      this.setDataValue("password", hash.digest("hex"));
     }
   }
 }, {
   // Options
   sequelize,
   tableName: "Users"
+});
+
+User.beforeCreate(async (user, options) => {
+  user.salt = randomBytes(16).toString("hex");
 });
 
 export default User;
