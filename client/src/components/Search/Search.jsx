bbf994825bb94b855bc949261e23a384aa7deb4c
Lisandro Veron <lisandro.veron@hotmail.com>
2024-04-25 16:27:09 -0300

    Client ->
    Added:
    - HTML elements in 'Search' route.
    - In 'Search' route, generic 'updateState' function that receives a useState 'set' function and an 'options' object as parameters, the purpose is to update the state with the appropriate 'set' function using the new values contained in 'options', avoiding repetition of logic in the code.
    
    Modified:
    - Minor fixes.

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -3,12 +3,12 @@ import {useEffect, useState} from "react";
 export default function Search() {
   const [searchParameters, setSearchParameters] = useState({
     countryCode: "ES",
-    hotel: null,
-    terminal: null,
+    hotel: "",
+    terminal: "",
     outbound: null,
-    adults: null,
-    children: null,
-    infants: null
+    adults: 0,
+    children: 0,
+    infants: 0
   });
   const [selectOptions, setSelectOptions] = useState({
     countries: [],
@@ -16,6 +16,14 @@ export default function Search() {
     terminals: []
   });
   const [currentHotelPage, setCurrentHotelPage] = useState(1);
+  const [fromTerminalToHotel, setFromTerminalToHotel] = useState(true);
+
+  const updateState = (setFunction, options) => {
+    setFunction((prevState) => ({
+      ...prevState,
+      ...options
+    }));
+  };
 
   useEffect(() => {
     fetch("/api/hotelbeds/cache/countries")
@@ -24,12 +32,7 @@ export default function Search() {
 
         return await response.json();
       })
-      .then((countries) => {
-        setSelectOptions((prevState) => ({
-          ...prevState,
-          countries: countries
-        }));
-      })
+      .then((countries) => updateState(setSelectOptions, {countries: countries}))
       .catch((error) => console.error("Error:", error.message));
   }, []);
 
@@ -40,16 +43,13 @@ export default function Search() {
         `&currentPage=${currentHotelPage}`
     )
       .then(async (response) => {
-        if (response.status !== 200) throw new Error(response.statusText);
+        if (response.status < 200 || response.status > 299) {
+          throw new Error(response.statusText);
+        };
 
         return await response.json();
       })
-      .then((hotels) => {
-        setSelectOptions((prevState) => ({
-          ...prevState,
-          hotels: hotels
-        }));
-      })
+      .then((hotels) => updateState(setSelectOptions, {hotels: hotels}))
       .catch((error) => console.error("Error:", error.message));
   }, [currentHotelPage, searchParameters.countryCode]);
 
@@ -63,17 +63,116 @@ export default function Search() {
 
         return await response.json();
       })
-      .then((terminals) => {
-        setSelectOptions((prevState) => ({
-          ...prevState,
-          terminals: terminals
-        }));
-      })
+      .then((terminals) => updateState(setSelectOptions, {terminals: terminals}))
       .catch((error) => console.error("Error:", error.message));
   }, [searchParameters.countryCode]);
 
+  const Hotel = () => {
+    return (
+      <>
+        <label htmlFor="hotel">Hotel:</label>
+        <select
+            id="hotel"
+            value={searchParameters.hotel}
+            required
+            onChange={(e) => {
+              updateState(setSearchParameters, {hotel: e.target.value});
+            }}>
+          {selectOptions.hotels.map((hotel) => (
+            <option key={`hotel-${hotel.code}`} value={hotel.code}>
+              {hotel.name}
+            </option>
+          ))}
+        </select>
+      </>
+    );
+  };
+
+  const Terminal = () => {
+    return (
+      <>
+        <label htmlFor="terminal">Terminal:</label>
+        <select
+            id="terminal"
+            value={searchParameters.terminal}
+            required
+            onChange={(e) => {
+              updateState(setSearchParameters, {terminal: e.target.value});
+            }}>
+          {selectOptions.terminals.map((terminal) => (
+            <option key={`terminal-${terminal.code}`} value={terminal.code}>
+              {terminal.content.description}
+            </option>
+          ))}
+        </select>
+      </>
+    );
+  };
+
   return (
     <>
+      <label htmlFor="countryCode">País:</label>
+      <select
+          id="countryCode"
+          value={searchParameters.countryCode}
+          required
+          onChange={(e) => {
+            updateState(setSearchParameters, {countryCode: e.target.value});
+          }}>
+        {selectOptions.countries.map((country) => (
+          <option key={`country-${country.code}`} value={country.code}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+
+      {fromTerminalToHotel
+        ? <><Terminal /> <Hotel /></>
+        : <><Hotel /> <Terminal /></>
+      }
+
+      <label htmlFor="outbound">Ida:</label>
+      <input
+          id="outbound"
+          type="datetime-local"
+          value={searchParameters.outbound}
+          required
+          onChange={(e) => {
+            updateState(setSearchParameters, {
+              outbound: e.target.value + ":00"
+            });
+          }} />
+
+      <label htmlFor="adults">Adultos:</label>
+      <input
+          id="adults"
+          type="number"
+          min="0"
+          value={searchParameters.adults}
+          required
+          onChange={(e) => {
+            updateState(setSearchParameters, {adults: e.target.value});
+          }} />
+      <label htmlFor="children">Niños:</label>
+      <input
+          id="children"
+          type="number"
+          min="0"
+          value={searchParameters.children}
+          required
+          onChange={(e) => {
+            updateState(setSearchParameters, {children: e.target.value});
+          }} />
+      <label htmlFor="infants">Infantes:</label>
+      <input
+          id="infants"
+          type="number"
+          min="0"
+          value={searchParameters.infants}
+          required
+          onChange={(e) => {
+            updateState(setSearchParameters, {infants: e.target.value});
+          }} />
     </>
   );
 };
